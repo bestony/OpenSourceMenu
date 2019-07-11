6c8274a392ec9e292310614b4ee0634dcf36eee4
Bestony <best.tony@foxmail.com>
2019-07-11 19:47:35 +0800

    feat: done homepage

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <PageHeader></PageHeader>
   </div>
 </template>
+<script>
+import PageHeader from "@/components/PageHeader.vue"
+export default {
+  components:{
+    PageHeader
+  }
+}
+</script>
 
 <style>
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="page-header">
+    <nav class="navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="@/assets/logo.png" width="112" height="28" />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">Home</a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">Community</a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">China Mainland</a>
+              <a class="navbar-item">Taiwan</a>
+              <a class="navbar-item">Hongkong</a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item">More</a>
+            </div>
+          </div>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">Company</a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">China Mainland</a>
+              <a class="navbar-item">Taiwan</a>
+              <a class="navbar-item">Hongkong</a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <b-button type="is-primary">Signup</b-button>
+              <b-button type="is-light">Login</b-button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
