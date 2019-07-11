ab5b44a1e27f278008802e194494c74e3ce7e3bf
Bestony <best.tony@foxmail.com>
2019-07-11 22:15:21 +0800

    feat: done area

---
+++ src/components/area/Area.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="area-item">
+    <h2 class="is-size-2 is-uppercase">{{this.area.flag}} {{this.area.lang[lang]}}</h2>
+    <hr />
+    <div class="container">
+      <div class="columns is-multiline">
+        <div class="column is-3 is-narrow" v-for="item in this.area.items" v-bind:key="item.id">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-16by9">
+                <img :src="item.image" :alt="item.lang[lang].name" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">{{item.lang[lang].name}}</p>
+                </div>
+              </div>
+              <div class="content">{{item.lang[lang].desc}}</div>
+            </div>
+            <footer class="card-footer">
+              <a :href="item.homepage" target="_blank" class="card-footer-item" v-show="item.homepage">
+                <b-icon icon="home" size="is-medium"></b-icon>
+              </a>
+              <a :href="item.email" target="_blank" class="card-footer-item" v-show="item.email">
+                <b-icon icon="email" size="is-medium"></b-icon>
+              </a>
+
+              <a :href="item.github" target="_blank" class="card-footer-item" v-show="item.github">
+                <b-icon icon="github-circle" size="is-medium"></b-icon>
+              </a>
+              <a :href="item.twitter" target="_blank" class="card-footer-item" v-show="item.twitter">
+                <b-icon icon="twitter" size="is-medium"></b-icon>
+              </a>
+              <a :href="item.meetup" target="_blank" class="card-footer-item" v-show="item.meetup">
+                <b-icon icon="meetup" size="is-medium"></b-icon>
+              </a>
+              <a :href="item.facebook" target="_blank" class="card-footer-item" v-show="item.facebook">
+                <b-icon icon="facebook-box" size="is-medium"></b-icon>
+              </a>
+              <a :href="item.googlegroup" target="_blank" class="card-footer-item" v-show="item.googlegroup">
+                <b-icon icon="google-circles-group" size="is-medium"></b-icon>
+              </a>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    area: Object
+  },
+  computed: {
+    lang() {
+      return this.$store.state.lang;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Area.vue
+++ src/views/Area.vue
@@ -1,32 +1,56 @@
 <template>
   <div class="area">
+    <div class="body">
+      <div class="container">
+        <h1 class="is-size-1 is-uppercase">{{areaData.flag}} {{areaData.lang[lang]}}</h1>
+        <hr>
+        <AreaItem v-for="item in areaData.areas" v-bind:key="item.id" :area="item"></AreaItem>
+      </div>
+    </div>
     <b-loading :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="true"></b-loading>
   </div>
 </template>
 
 <script>
+import AreaItem from '@/components/area/Area.vue';
 export default {
   props: ["mode", "area"],
+  components:{
+    AreaItem
+  },
   data() {
     return {
       isLoading: true,
       isFullPage: true,
-      areaData:null
+      areaData: null
     };
   },
   beforeMount() {
     let url = `../data/${this.mode}/${this.area}.json`;
     fetch(url)
       .then(function(response) {
-          return response.text();
-      }).then(res => {
-        this.areaData = JSON.parse(res)
-        this.isLoading = false
-      }).catch(res => {
-        this.$router.replace({
-          name:"blank"
-        })
+        return response.text();
       })
+      .then(res => {
+        this.areaData = JSON.parse(res);
+        this.isLoading = false;
+      })
+      .catch(res => {
+        this.$router.replace({
+          name: "blank"
+        });
+      });
+  },
+  computed: {
+    lang() {
+      return this.$store.state.lang;
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.body{
+  margin:50px 0px; 
+}
+</style>
