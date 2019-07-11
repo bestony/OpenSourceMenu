8f2a00855c3cf576dcb2edd4cc72373e0a816994
Bestony on Travis (from Travis CI) <deploy@travis-ci.org>
2019-07-11 13:10:35 +0000

    Deploy opensourcelists.com to github.com/bestony/opensourcelists.com.git:gh-pages

---
+++ js/community.2115d9a1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["community"],{8838:function(n,t,e){"use strict";e.r(t);var c=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"community"})},s=[],u=e("2877"),i={},l=Object(u["a"])(i,c,s,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=community.2115d9a1.js.map
--- js/company.8a341349.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["company"],{8799:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},o=[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("h1",[n._v("🏢 Copmany")])])}],r={components:{}},a=r,l=t("2877"),s=Object(l["a"])(a,c,o,!1,null,null,null);e["default"]=s.exports}}]);
-//# sourceMappingURL=company.8a341349.js.map
