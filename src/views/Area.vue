2a3e764ad86cd3c05e1b89f2586a7c6d70a441e4
Bestony <best.tony@foxmail.com>
2019-07-11 21:08:03 +0800

    fix: fix for ci

--- src/views/Area.vue
+++ src/views/Area.vue
@@ -1,13 +1,30 @@
 <template>
-    <div class="area"></div>
+  <div class="area">
+    <b-loading :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="true"></b-loading>
+  </div>
 </template>
 
 <script>
 export default {
-    props:['mode','area'],
-    created(){
-        console.log("mode",this.mode);
-        console.log("area",this.area);
-    }
-}
+  props: ["mode", "area"],
+  data() {
+    return {
+      isLoading: true,
+      isFullPage: true,
+      areaData:null
+    };
+  },
+  beforeMount() {
+    let url = `../data/${this.mode}/${this.area}.json`;
+    fetch(url)
+      .then(function(response) {
+          return response.text();
+      }).then(res => {
+        this.areaData = JSON.parse(res)
+        this.isLoading = false
+      })
+  },
+  created() {
+  }
+};
 </script>
