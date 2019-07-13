2d4820da53b88d3ab562fc89107760058243943d
Bestony <best.tony@foxmail.com>
2019-07-14 03:11:53 +0800

    feat: rename to oepnsource menu

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -2,7 +2,7 @@
 <footer class="footer">
   <div class="content has-text-centered">
     <p>
-      <strong>Opensource Lists</strong> by <a href="https://www.ixiqin.com">Bestony (HuanCheng Bai)</a>. The source code is licensed
+      <strong>Opensource Menu</strong> by <a href="https://www.ixiqin.com">Bestony (HuanCheng Bai)</a>. The source code is licensed
       <a href="https://github.com/bestony/opensourcelists.com/blob/master/LICENSE">AGPL</a>. The website content
       is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
     </p>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <section class="hero is-primary is-medium has-text-centered hero-content">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title is-size-1">Opensource Lists</h1>
+          <h1 class="title is-size-1">Opensource Menu</h1>
           
           <h2 class="subtitle">a website to find opensource community or opensource company</h2>
           <router-link to="/community/tw"><b-button type="is-light" class="is-size-4" outlined>Find By Your Location</b-button></router-link>
@@ -12,10 +12,10 @@
     </section>
     <section>
       <div class="container info-content">
-        <h1 class="is-size-1 has-text-weight-semibold">Opensource Lists</h1>
+        <h1 class="is-size-1 has-text-weight-semibold">Opensource Menu</h1>
         <p
           class="is-size-3"
-        >Opensource Lists is a Website to find Your Local Opensource community or opensource Company</p>
+        >Opensource Menu is a Website to find Your Local Opensource community or opensource Company</p>
       </div>
     </section>
     <section class="count-content has-background-light">
